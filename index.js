6ae31834ba157121cd2b617e1338eb33d2142797
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-28 08:56:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function subtract (a,b){
 function multiply (a,b){
   return a * b
 }
-function add (a,b){
-  return a + b
+function divide (a,b){
+  return a / b
 }
 function add (a,b){
   return a + b
